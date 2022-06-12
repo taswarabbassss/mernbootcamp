a2540dfa882ad53d5ae0fdbebc38ed31f7f8003a
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-12 18:12:04 +0500

    Errors During Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknown errors: Don't leak the error Details
+  } else {
+    // 1) Log the Error
+    console.error('Error 🔥', err);
+    // 2) Send generic Message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong! 🙄'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
