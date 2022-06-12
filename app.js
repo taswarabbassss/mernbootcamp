6d2ce151921227baaa5245225a7b9ce1f64643e1
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-12 11:41:42 +0500

    Refectoring Error Handlers

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const { del } = require('express/lib/application');
 const res = require('express/lib/response');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const req = require('express/lib/request');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -33,20 +36,13 @@ app.all('*', (req, res, next) => {
   //   message: `Cant find ${req.originalUrl} on this server!`
   // });
 
-  const err = new Error(`Cant find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  // const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
 
