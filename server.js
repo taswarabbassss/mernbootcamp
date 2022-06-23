48d48857a80df55ef0029628f6b366bdb7840c00
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-23 17:14:13 +0500

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION!  💥 Shutting down.... ');
+  process.exit(1);
+});
+
 const app = require('./app');
 
 //console.log(process.env);
