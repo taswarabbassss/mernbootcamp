73886ed899471830e89c49df184e0ecd7fd00de3
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-23 11:41:51 +0500

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = err => {
   return new AppError(message, 404);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name;
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -40,13 +47,12 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else {
     //if (process.env.NODE_ENV === 'production')
-    console.log(`ERROR SECOND IS : ${err.name}`);
     //MAKING DEEP COPY OF ERROR OBJECT
     let error = JSON.parse(JSON.stringify(err));
     // let error = { ...err };  THIS IS MAKING JUST SHALLOW COPY OF ERROR OBJECT
-    console.log(`ERROR FIRST IS : ${error.name}`);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    
     sendErrorProd(error, res);
   }
 };
