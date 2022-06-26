631fb5c4f7ada8e9172175e6a22c5e5eba32d7d3
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-26 17:01:53 +0500

    136: Sending Email with Nodemailer ( Email Sending Process)

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const User = require('./../models/userModel');
 const jwt = require('jsonwebtoken');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+const sendEmail = require('../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -117,6 +118,36 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
   // 3) SEND IT TO THE USER'S EMAIL
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and confirm password to : ${resetURL}. \n If you didn't forget your password, please ignore this email! `;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 Minutes) ',
+      message: message
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email! '
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the Email. Please try again later!',
+        500
+      )
+    );
+  }
+
   next();
 });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 //Rourtes for User
 router
