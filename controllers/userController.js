26e66d59c306e2be58974e68c0442fc7a25c4501
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-29 18:35:37 +0500

    139: Updating the Current User: Data ( 'name', 'email', 'picture')

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 //USER HANDLERS
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -8,7 +19,7 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   // const query = Tour.find()
   //   .where('duration')
   //   .equals(5)
-  //   .where('difficulty')
+  //  / .where('difficulty')
   //   .equals('easy');
 
   res.status(200).json({
@@ -16,6 +27,34 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users: users
+    }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route  is not for password updates. Please use /updateMyPassword . ',
+        400
+      )
+    );
+  }
+
+  // 2) Filter out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 2) Update User Document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
     }
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,6 +17,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 //Rourtes for User
 router
   .route('/')
