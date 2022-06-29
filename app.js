a903b2f1de20fd67fe62da6bfb1d7d3bf2c16dd7
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-30 01:21:19 +0500

    143 : Implementing Rate Limiting ( To avoid too many request, or bruite force attack )

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { del } = require('express/lib/application');
 const res = require('express/lib/response');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -12,11 +12,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1) MIDDLEWARES
+//1) GLOBAL MIDDLEWARES
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
