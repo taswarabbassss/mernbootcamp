2b94adbc160aa206d1f0c2d27e736b41190b16e8
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-06-30 17:42:26 +0500

    145 : Data Sanitization ( From NoSQL query Injection by 'express-mongo-sanitize' and Cross-site-scripting by 'xss-clean')

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const res = require('express/lib/response');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,7 +39,14 @@ app.use('/api', limiter);
 
 // iv) Body parser, reading data from body into req.body
 
-app.use(express.json()); //Limiting amount of Data that comes in the body
+app.use(express.json({ limit: '10kb' })); //Limiting amount of Data that comes in the body
+
+// Data Sanitization against NoSQL query injection
+
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss())
 
 // v) Serving Static Files
 
