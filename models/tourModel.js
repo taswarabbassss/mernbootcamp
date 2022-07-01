e45c8af9eb56a2d9e4918da2e5713590d467b830
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-01 10:27:05 +0500

    151 : Modelling Tour Guides : Embedding ( As a Example )

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
+const User = require('./userModel');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -94,7 +96,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -109,6 +112,15 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MOIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Middleware for guides array: To fetch the users data from users collection to tours Collection by their ID's
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromise = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromise);
+
   next();
 });
 
