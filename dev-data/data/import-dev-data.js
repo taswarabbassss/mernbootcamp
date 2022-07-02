ffeecd05b1ebad6915e5b5bd97a40659870f7bc9
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-02 18:57:30 +0500

    166 : Importing Reviews and User Data ( From users.json and reviews.json file )

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 //console.log(process.env);
 
@@ -26,11 +28,17 @@ mongoose
 
 // READ JSON FILE trour-simple.json
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 
 // IMPORT DATA INTO THE DATABASE
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data Successfully Loaded😀');
   } catch (err) {
     console.log(err);
@@ -42,6 +50,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Successfully Deleted😀');
   } catch (err) {
     console.log(err);
