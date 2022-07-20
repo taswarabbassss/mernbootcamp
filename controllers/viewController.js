fe28bd6140f5028b758b80910bbed050217904b0
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-20 23:37:21 +0500

    188 : Building the Login Screen

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get Tour data from the collection
   const tours = await Tour.find();
   // 2) Build Template
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour ( including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -34,3 +34,9 @@ exports.getTour = catchAsync(async (req, res) => {
       tour
     });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
