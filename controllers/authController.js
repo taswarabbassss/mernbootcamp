6e40d910317dd6060dd59e67e637abeb4fc5cfe6
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-21 03:55:35 +0500

    190 : Logging in Users with Our API - Part 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -112,6 +112,33 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+// Only for rendered pages, there will be no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) Verify the Token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
+
+    // 2) Check if the user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3) Check if the user changed password after the token was issued
+    if (currentUser.changesPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    return next();
+  }
+  next();
+});
+
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles: ['admin', 'lead-guide'].  if role ='user' don't have permission to access.
--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 const login = async (email, password) => {
-  console.log(email, password);
-
   try {
     const res = await axios({
       method: 'POST',
@@ -11,9 +9,15 @@ const login = async (email, password) => {
         password
       }
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      alert('Logged In Successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
