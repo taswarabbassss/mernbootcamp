e14a65d16402d2a71880bb1b600a468cdb8fea65
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-20 10:52:14 +0500

    181 : Setting up the Project Structure

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const req = require('express/lib/request');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -81,25 +82,8 @@ app.use((req, res, next) => {
 });
 
 // 2)ROUTE HANDLERS
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Tasawar'
-  });
-});
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
