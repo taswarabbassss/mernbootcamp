e34b44992f9bb0917eb4a35bdfda74bd55a0a20a
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-22 11:10:45 +0500

    196 : Updating User Data with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,12 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
-
+import { updateData } from './updateSettings';
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATIONS
 if (mapBox) {
@@ -23,3 +24,12 @@ if (loginForm)
   });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', e => {
+    console.log('HERE');
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateData(name, email);
+  });
