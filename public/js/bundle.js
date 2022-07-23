d3c184ff90eee793473fdb8df441e331d2830e89
taswarabbassss <106326542+taswarabbassss@users.noreply.github.com>
2022-07-23 18:59:02 +0500

    203 : Adding Image Uploads to Form

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -11530,12 +11530,12 @@ if (loginForm) loginForm.addEventListener('submit', function (e) {
 if (logOutBtn) logOutBtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var email = document.getElementById('email').value;
-  var name = document.getElementById('name').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  console.log(form);
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit',
 /*#__PURE__*/
@@ -11607,7 +11607,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58206" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59107" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
